49d931fb6862d31e5691c09067eae9cfb21db0a9
MarcBertu <marcbertu@gmail.com>
2023-05-16 10:40:00 +0200

    Ajout du model Credentials

---
+++ server/src/models/Credential.ts
@@ -0,0 +1,44 @@
+import { DataTypes, INTEGER, STRING } from "sequelize";
+import db from '../db/connection';
+
+const Credential = db.define('Credentials', {
+    idCredentials: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        unique: true,
+        autoIncrement: true,
+        allowNull : false,
+    },
+    login: {
+        type: DataTypes.STRING(45),
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true,
+            notEmpty: true,
+            notNull: true,
+            len: [5,45]
+        }
+    },
+    hashPassword: {
+        type: DataTypes.STRING(128),
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+            notNull: true,
+        }
+    },
+    typeUser: {
+        type: DataTypes.SMALLINT,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+            notNull: true,
+            len: [1,1],
+        }
+    }
+}, {
+    timestamps: false
+})
+
+export default Credential;
