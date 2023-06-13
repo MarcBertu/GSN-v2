37b9cfb34d0245326770aefccb2a1e2c8823421c
MarcBertu <marcbertu@gmail.com>
2023-06-13 20:22:38 +0200

    Fix: crash du serveur (en local)

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,32 @@
-import Server from "./models/server";
-import dotenv from 'dotenv'
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import routeCreds from './routes/Credentials';
+import routeSite from './routes/Site';
+import routeTask from './routes/Task';
+import routeMail from './routes/Mail';
+import db from './db/connection';
 
-// Configuration des variables ambiantes
 dotenv.config();
 
-const server = new Server();
+const app: Express = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).send('OK');
+});
+
+app.use('/credentials', routeCreds);
+app.use('/mail', routeMail);
+app.use('/site', routeSite);
+app.use('/task', routeTask);
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running`);
+});
+
+db.authenticate();
